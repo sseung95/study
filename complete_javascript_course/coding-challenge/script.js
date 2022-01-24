6078715bf2b84aca92d044c894cdf3ceba0b0b8b
sseung95 <seung3657@gmail.com>
2022-01-24 15:16:00 +0900

    📦 NEW: 보너스 연습문제 추가

--- complete_javascript_course/coding-challenge/script.js
+++ complete_javascript_course/coding-challenge/script.js
@@ -242,3 +242,24 @@ const convertCamelCase = function () {
 };
 
 document.querySelector('button').addEventListener('click', convertCamelCase);
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const flightArr = flights.split('+');
+
+const getCode = (str) => str.slice(0, 3).toUpperCase();
+
+for (const flight of flightArr) {
+  const [type, from, to, time] = flight.split(';');
+  const result = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(45);
+
+  console.log(result);
+}
