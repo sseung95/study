538905ba99db0fc5eac91b08ba4636caaa36d90d
sseung95 <seung3657@gmail.com>
2022-05-27 18:08:14 +0900

    feat: redux 사용한 숫자 증가, 감소 기능

---
+++ react/redux-practice/redux-demo.js
@@ -0,0 +1,31 @@
+const redux = require('redux');
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  // 최신 상태 상태값이 변경될때마다 트리거되고, 스토어의 상태값을 받을 수 ㅣ있다.
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });
