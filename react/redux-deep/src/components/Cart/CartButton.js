6b525133c730ce52e7056707b547a12e4c49ca7e
sseung95 <seung3657@gmail.com>
2022-06-03 01:24:15 +0900

    feat: 장바구니 토글 기능
    오른쪽 상단 My Cart 버튼 클릭 시 장바구니 토글 기능 구현

--- react/redux-deep/src/components/Cart/CartButton.js
+++ react/redux-deep/src/components/Cart/CartButton.js
@@ -1,10 +1,17 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
   const items = useSelector((state) => state.cart.items);
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>{items.length}</span>
     </button>
--- react/redux-deep/src/store/index.js
+++ react/redux-deep/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import uiSlice from './ui-slice';
 import cartReducer from './cart';
 import productsReducer from './products';
 
 const store = configureStore({
   reducer: {
+    ui: uiSlice.reducer,
     cart: cartReducer,
     products: productsReducer,
   },
