21d42bddb33fe70bdc6d50c7c6243146c2057962
sseung95 <seung3657@gmail.com>
2022-06-07 19:11:52 +0900

    feat: 인용문 상세 내용 HighlightedQuote 컴포넌트로 렌더링

--- react/react-router-project/src/pages/QuoteDetail.js
+++ react/react-router-project/src/pages/QuoteDetail.js
@@ -2,14 +2,33 @@ import { useParams } from 'react-router';
 import { Route } from 'react-router';
 
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Max',
+    text: 'Learning React is fun!',
+  },
+  {
+    id: 'q2',
+    author: 'Seung',
+    text: 'Learning React is great!',
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote author={quote.author} text={quote.text} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
