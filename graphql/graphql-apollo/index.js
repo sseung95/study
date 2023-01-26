90b98c7c68d7ab7c21482b214d7d256ddf6fece0
sseung95 <seung3657@gmail.com>
2023-01-26 20:21:36 +0900

    Apollo 서버 구축 및 Query 구현

---
+++ graphql/graphql-apollo/index.js
@@ -0,0 +1,49 @@
+const database = require('./database');
+const { ApolloServer, gql } = require('apollo-server');
+const typeDefs = gql`
+  type Query {
+    teams: [Team]
+    team(id: Int): Team
+    equipments: [Equipment]
+    supplies: [Supply]
+  }
+  type Team {
+    id: Int
+    manager: String
+    office: String
+    extension_number: String
+    mascot: String
+    cleaning_duty: String
+    project: String
+    supplies: [Supply]
+  }
+  type Equipment {
+    id: String
+    used_by: String
+    count: Int
+    new_or_used: String
+  }
+  type Supply {
+    id: String
+    team: Int
+  }
+`;
+const resolvers = {
+  Query: {
+    teams: () =>
+      database.teams.map((team) => {
+        team.supplies = database.supplies.filter(
+          (supply) => supply.team === team.id
+        );
+        return team;
+      }),
+    team: (parent, args, context, info) =>
+      database.teams.find((team) => team.id === args.id),
+    equipments: () => database.equipments,
+    supplies: () => database.supplies,
+  },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
